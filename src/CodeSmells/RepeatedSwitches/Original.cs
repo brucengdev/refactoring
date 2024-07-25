a573626c529ffdf95b14b7ef49c2bdcc51191fe2
brucengdev <brucengdev@gmail.com>
2024-07-25 13:07:28 +0700

    Added example on repeated switches.

---
+++ src/CodeSmells/RepeatedSwitches/Original.cs
@@ -0,0 +1,59 @@
+﻿/**
+ * This code smell is when you see repeated switches.
+ * It is troublesome because whenever you add a new switch case, you have
+ * to find all the switches to add the case, and you might miss some.
+ * 
+ * The solution to this is to use polymorphism, there should be only
+ * one switch case to create an instance of the correct subclass, depending
+ * on the switch value.
+ **/
+
+namespace RepeatedSwitches.Original
+{
+    public enum JobType
+    {
+        Student,
+        Doctor,
+        Programmer
+    }
+    public class Professional
+    {
+        private readonly JobType _jobType;
+        private readonly string _name;
+        public Professional(JobType jobType, string name)
+        {
+            _jobType = jobType;
+            _name = name;
+        }
+
+        public string Greet()
+        {
+            switch(_jobType)
+            {
+                case JobType.Student:
+                    return $"Hello, student {_name}!";
+                case JobType.Doctor:
+                    return $"Hello, Dr. {_name}!";
+                case JobType.Programmer:
+                    return $"Hello, {_name} the engineer!";
+                default: 
+                    return $"Hello, {_name}!";
+            }
+        }
+
+        public float GetSalary()
+        {
+            switch(_jobType)
+            {
+                case JobType.Student:
+                    return 0;//unemployed
+                case JobType.Doctor:
+                    return 100000;
+                case JobType.Programmer:
+                    return 5000;
+                default:
+                    return 0;
+            }
+        }
+    }
+}
---
+++ src/CodeSmells/RepeatedSwitches/Refactored.cs
@@ -0,0 +1,65 @@
+﻿/**
+ * In this refactored version, we use polymorphism to change the behavior
+ * depending on the subclass being used.
+ * The repeated switches are changed to only one switch to create an instance
+ * of the correct subclass.
+ * Whenever you add a new type of professional, you only need to add one new switch case
+ * and add a new class for the new type of professional, with all new behaviors required
+ * by the abstract base class.
+ **/
+
+namespace RepeatedSwitches.Refactored
+{
+    public enum JobType
+    {
+        Student,
+        Doctor,
+        Programmer
+    }
+    public abstract class Professional
+    {
+        public static Professional Create(JobType jobType, string name)
+        {
+            switch(jobType)
+            {
+                case JobType.Student:
+                    return new Student(name);
+                case JobType.Doctor:
+                    return new Doctor(name);
+                case JobType.Programmer:
+                    return new Programmer(name);
+                default:
+                    throw new ArgumentException("Invalid job type");
+            }
+        }
+        public abstract string Greet();
+        public abstract float GetSalary();
+    }
+
+    public class Student : Professional
+    {
+        private readonly string _name;
+        public Student(string name) { _name = name; }
+        public override float GetSalary() => 0;//unemployed
+
+        public override string Greet() => $"Hello, student {_name}!";
+    }
+
+    public class Doctor : Professional
+    {
+        private readonly string _name;
+        public Doctor(string name) { _name = name; }
+        public override float GetSalary() => 10000;
+
+        public override string Greet() => $"Hello, Dr. {_name}!";
+    }
+
+    public class Programmer : Professional
+    {
+        private readonly string _name;
+        public Programmer(string name) { _name = name; }
+        public override float GetSalary() => 5000;
+
+        public override string Greet() => $"Hello, {_name} the engineer!";
+    }
+}
