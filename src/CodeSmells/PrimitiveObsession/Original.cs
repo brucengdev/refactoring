f0ab7c1236b85d7772d9442e978d85c03c31e49f
brucengdev <brucengdev@gmail.com>
2024-07-26 12:40:39 +0700

    Added example of primitive obsession.

---
+++ src/CodeSmells/PrimitiveObsession/Original.cs
@@ -0,0 +1,40 @@
+﻿namespace PrimitiveObsession.Original
+{
+    /**
+     * An example of primitive obsession.
+     * Instead of creating your own type to handle money, you use primitive like float
+     * Because the primitive lacks context and central methods, you have no idea what it does
+     * and you need to remember to process the primitive every time it is used.
+     * 
+     * Other examples: use strings for telephone numbers, multiple numbers for coordinates,...
+     * This is not necessary as modern languages have powerful type system that can be used
+     * to create domain specific types.
+     **/
+    public enum Currency
+    {
+        USD,
+        SGD,
+        VND
+    }
+    public class PrimitiveObsessionExample
+    {
+        
+        private Dictionary<Currency, float> exchangeRates;
+        public float getTotalMoney((float, Currency)[] payments, 
+            Currency targetCurrency)
+        {
+            float total = 0;
+            foreach(var (amount, currency) in payments)
+            {
+                var convertedAmount = amount * exchangeRates[currency];
+                total += convertedAmount;
+            }
+            return total / exchangeRates[targetCurrency];
+        }
+
+        public string PrintMoney(float amount, Currency currency)
+        {
+            return string.Format("{0:F2}{1}", amount, currency);
+        }
+    }
+}
---
+++ src/CodeSmells/PrimitiveObsession/Refactored.cs
@@ -0,0 +1,61 @@
+﻿namespace PrimitiveObsession.Refactored
+{
+    /**
+     * An example of primitive obsession.
+     * In this refactored example, instead of using float for money, we
+     * create a dedicated type Money that handles everything related to money.
+     * This way all processing needed for this type is encapsulated in one place.
+     * And the users of the type can just use it for Money without having to take 
+     * care of currency related processing and conversions.
+     * 
+     * The displaying of money is also consolidated in the class.
+     **/
+    public enum Currency
+    {
+        USD,
+        SGD,
+        VND
+    }
+
+    public class Money
+    {
+        private static Dictionary<Currency, float> exchangeRates;
+        private readonly Currency _currency;
+        private readonly float _amount;
+
+        public Money(Currency currency, float amount)
+        {
+            _currency = currency;
+            _amount = amount;
+        }
+
+        public float Amount {  get {  return _amount; } }
+        public Currency Currency { get {  return _currency; } }
+
+        public Money Add(Money other, Currency targetCurrency)
+        {
+            var uniAmount = Amount * exchangeRates[Currency] + other.Amount * exchangeRates[other.Currency];
+            return new Money(targetCurrency, uniAmount / exchangeRates[targetCurrency]);
+        }
+
+        //centralize display of money
+        public override string ToString()
+        {
+            return string.Format("{0:F2}{1}", Amount, Currency);
+        }
+    }
+
+    public class PrimitiveObsessionExample
+    {
+        
+        public Money getTotalMoney(Money[] payments, Currency targetCurrency)
+        {
+            var total = new Money(targetCurrency, 0);
+            foreach(var payment in payments)
+            {
+                total = total.Add(payment, targetCurrency);
+            }
+            return total;
+        }
+    }
+}
