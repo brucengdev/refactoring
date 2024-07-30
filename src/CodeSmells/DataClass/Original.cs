5a127fc6298d383ecf0edc06de8bd3be72c6d6e5
brucengdev <brucengdev@gmail.com>
2024-07-30 12:56:59 +0700

    Added example on data classes

---
+++ src/CodeSmells/DataClass/Original.cs
@@ -0,0 +1,31 @@
+﻿/**
+ * Data classes are dumb classes that are manipulated everywhere
+ * They are an issue when the behaviors are in the wrong place, they
+ * are in the clients using the class instead of inside the class itself.
+ * 
+ * Solve this with
+ * - Encapsulate Record
+ * - Remove setting method
+ * - Move behaviors from clients to the class for reuse
+ **/
+
+namespace DataClass.Original
+{
+    public class Profile
+    {
+        public string Name { get;set; }
+        public int Age { get;set; }
+        public string Salutation { get;set; }
+    }
+
+    public class DataClassExample
+    {
+        public void PrintProfile()
+        {
+            var profile = new Profile();
+            Console.WriteLine("Name: {0}", profile.Name);
+            Console.WriteLine("Age: {0}", profile.Age);
+            Console.WriteLine("Salutation: {0}", profile.Salutation);
+        }
+    }
+}
---
+++ src/CodeSmells/DataClass/Refactored.cs
@@ -0,0 +1,41 @@
+﻿
+
+using DataClass.Original;
+
+/**
+ * In this refactored version, the behaviors are moved into the class
+ * so the class is not a dumb data class anymore.
+ **/
+
+namespace DataClass.Refactored
+{
+    public class Profile
+    {
+        private string Name { get;set; }
+        private int Age { get;set; }
+        private string Salutation { get;set; }
+
+        public Profile(string name, int age, string salutation)
+        {
+            Name = name;
+            Age = age;
+            Salutation = salutation;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("Name: {0}", Name);
+            Console.WriteLine("Age: {0}", Age);
+            Console.WriteLine("Salutation: {0}", Salutation);
+        }
+    }
+
+    public class DataClassExample
+    {
+        public void PrintProfile()
+        {
+            var profile = new Profile("Tom", 22, "Mr.");
+            profile.Print();
+        }
+    }
+}
