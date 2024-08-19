1b995db7ee45e11d17a68e4b7cae97ec6d5cfe68
brucengdev <brucengdev@gmail.com>
2024-08-19 12:58:09 +0700

    Added example on Split Phase refactoring

---
+++ src/Refactorings/SplitPhase/Original.cs
@@ -0,0 +1,29 @@
+﻿namespace SplitPhase.Original
+{
+    public class PriceCalculator
+    {
+        private static Dictionary<string, float> _productPrices = new()
+        {
+            ["Cup"] = 2.5f 
+        };
+        public static float CalculateOrderPrice(string orderString)
+        {
+            /**
+             * This operation is doing two things
+             * First parse the order string
+             * Then calculate the order price
+             * 
+             * It can be valuable to split this into two phases.
+             **/
+            var orderData = orderString.Split("-");
+            var productPrice = _productPrices[orderData[0]];
+            var orderPrice = Convert.ToInt32(orderData[1]) * productPrice;
+            return orderPrice;
+        }
+
+        public static void Main()
+        {
+            Console.WriteLine("Price = {0}", CalculateOrderPrice("Cup-25"));
+        }
+    }
+}
---
+++ src/Refactorings/SplitPhase/Refactored.cs
@@ -0,0 +1,49 @@
+﻿namespace SplitPhase.Refactored
+{
+
+    internal class OrderData
+    {
+        public string ProductName;
+        public int OrderQuantity;
+    }
+    public class PriceCalculator
+    {
+        private static Dictionary<string, float> _productPrices = new()
+        {
+            ["Cup"] = 2.5f 
+        };
+        public static float CalculateOrderPrice(string orderString)
+        {
+            /**
+             * Calculating order price is splitted into two phases
+             * Using OrderData intermediate data structure to join two phases.
+             * The second phase should only use function parameters that are specific to the
+             * second phase (not shown in this example).
+             **/
+            var orderData = ParseOrder(orderString);
+            var orderPrice = CalculateProductPrice(orderData);
+            return orderPrice;
+        }
+
+        private static float CalculateProductPrice(OrderData orderData)
+        {
+            var productPrice = _productPrices[orderData.ProductName];
+            var orderPrice = orderData.OrderQuantity * productPrice;
+            return orderPrice;
+        }
+
+        private static OrderData ParseOrder(string orderString)
+        {
+            var parsed = orderString.Split("-");
+            var orderData = new OrderData();
+            orderData.ProductName = parsed[0];
+            orderData.OrderQuantity = Convert.ToInt32(parsed[1]);
+            return orderData;
+        }
+
+        public static void Main()
+        {
+            Console.WriteLine("Price = {0}", CalculateOrderPrice("Cup-25"));
+        }
+    }
+}
