13f3ae8fa5647efb70117f06bc810cc43045c522
brucengdev <brucengdev@gmail.com>
2024-08-22 13:12:28 +0700

    Added example on encapsulate record.

---
+++ src/Refactorings/EncapsulateRecord/Original.cs
@@ -0,0 +1,32 @@
+﻿/**
+ * This example does not use some C# features to demonstrate
+ * how it works in other languages.
+ **/
+namespace EncapsulateRecord.Original
+{
+    public class ProfileRecord
+    {
+        public string Name;
+        public string Email;
+    }
+    public class IssueReporter
+    {
+        private ProfileRecord ContactPerson = new ProfileRecord
+        {
+            Name = "Jerry",
+            Email = "jerry@somecorp.com"
+        };
+
+        public void ReportIssue(string issue)
+        {
+            string title = $"Dear {ContactPerson.Name}, there has been an issue";
+            string message = $"Details about the issue: {issue}";
+            SendEmail(ContactPerson.Email, title, message);
+        }
+
+        private void SendEmail(string email, string title, string message)
+        {
+            //send the email to the security personel
+        }
+    }
+}
---
+++ src/Refactorings/EncapsulateRecord/Refactored.cs
@@ -0,0 +1,53 @@
+﻿
+using System.Net.Http.Headers;
+
+/**
+ * This example does not use some C# features to demonstrate
+ * how it works in other languages.
+ * 
+ * Wrap the record in a class and gradually move things from the record to the class
+ * Check OneNote note on the steps to encapsulate the record.
+ **/
+namespace EncapsulateRecord.Refactored
+{
+    public class Profile
+    {
+        private string _name;
+        private string _email;
+
+        public Profile(string name, string email)
+        {
+            _name = name;
+            _email = email;
+        }
+
+        public string GetName() { return _name; }
+        public string GetEmail() { return _email; }
+    }
+    public class IssueReporter
+    {
+        private Profile ContactPersonProfile;
+
+        public IssueReporter()
+        {
+            ContactPersonProfile = new("Jerry", "jerry@somecorp.com");
+        }
+
+        private Profile GetContactPersonProfile()
+        {
+            return ContactPersonProfile;
+        }
+
+        public void ReportIssue(string issue)
+        {
+            string title = $"Dear {GetContactPersonProfile().GetName()}, there has been an issue";
+            string message = $"Details about the issue: {issue}";
+            SendEmail(GetContactPersonProfile().GetEmail(), title, message);
+        }
+
+        private void SendEmail(string email, string title, string message)
+        {
+            //send the email to the security personel
+        }
+    }
+}
