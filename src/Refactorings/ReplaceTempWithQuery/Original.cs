224b1a252dd79a168c46050e3676b195f078beca
brucengdev <brucengdev@gmail.com>
2024-09-02 23:13:45 +0700

    Added example of replace temps with query.

---
+++ src/Refactorings/ReplaceTempWithQuery/Original.cs
@@ -0,0 +1,43 @@
+﻿namespace ReplaceTempWithQuery.Refactored
+{
+    public class Item
+    {
+        public float Price { get;set; }
+    }
+    public class Order
+    {
+        private int _quantity;
+        private Item _item;
+        public Order(int quantity, Item item)
+        {
+            _quantity = quantity;
+            _item = item;
+        }
+
+        private double _basePrice
+        {
+            get
+            {
+                return _quantity * _item.Price;
+            }
+        }
+
+        private double _discountFactor
+        {
+            get
+            {
+                var value = 0.98;
+                if(_basePrice > 1000)
+                {
+                    value -= 0.03;
+                }
+                return value;
+            }
+        }
+
+        public float GetPrice()
+        {
+            return (float)(_basePrice * _discountFactor);
+        }
+    }
+}
---
+++ src/Refactorings/ReplaceTempWithQuery/Refactored.cs
@@ -0,0 +1,28 @@
+﻿namespace ReplaceTempWithQuery.Original
+{
+    public class Item
+    {
+        public float Price { get;set; }
+    }
+    public class Order
+    {
+        private int _quantity;
+        private Item _item;
+        public Order(int quantity, Item item)
+        {
+            this._quantity = quantity;
+            this._item = item;
+        }
+
+        public float GetPrice()
+        {
+            var basePrice = _quantity * _item.Price;
+            var discountFactor = 0.98;
+            if(basePrice > 1000)
+            {
+                discountFactor -= 0.03;
+            }
+            return (float)(basePrice * discountFactor);
+        }
+    }
+}
