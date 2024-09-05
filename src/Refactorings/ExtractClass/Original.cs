6a84687d4bac58ea9cac9a21927710cdaf51a00a
brucengdev <brucengdev@gmail.com>
2024-09-05 12:36:40 +0700

    Added example of Extract Class.

---
+++ src/Refactorings/ExtractClass/Original.cs
@@ -0,0 +1,14 @@
+﻿namespace ExtractClass.Original
+{
+    public class Person
+    {
+        public string Name { get;set; }
+        public string TelephoneNumber { get{
+                return $"{OfficeAreaCode}{OfficeNumber}";
+            } }
+
+        public string OfficeAreaCode { get; set; }
+
+        public string OfficeNumber { get;set; }
+    }
+}
---
+++ src/Refactorings/ExtractClass/Refactored.cs
@@ -0,0 +1,46 @@
+﻿namespace ExtractClass.Refactored
+{
+    internal class TelephoneNumber
+    {
+        public string AreaCode { get;set; }
+        public string Number { get;set; }
+
+        public string PhoneNumber
+        {
+            get
+            {
+                return $"{AreaCode}{Number}";
+            }
+        }
+    }
+    public class Person
+    {
+        private TelephoneNumber _telephoneNumber = new();
+
+        public string Name { get;set; }
+        public string TelephoneNumber { 
+            get{
+                return _telephoneNumber.PhoneNumber;
+            } 
+        }
+
+        public string OfficeAreaCode { 
+            get{
+                return _telephoneNumber.AreaCode;
+            } 
+            set{
+                _telephoneNumber.AreaCode = value;
+            } 
+        }
+
+        public string OfficeNumber { 
+            get{
+                return _telephoneNumber.Number;
+            }
+            set
+            {
+                _telephoneNumber.Number = value;
+            }
+        }
+    }
+}
