c5f0d61167f4340210a92b372d90ae39cebdedad
brucengdev <brucengdev@gmail.com>
2024-09-05 13:12:02 +0700

    Added example of Inline Class.

---
+++ src/Refactorings/InlineClass/Original.cs
@@ -0,0 +1,39 @@
+﻿namespace InlineClass.Refactored
+{
+    public class Shipment
+    {
+        private string _shippingCompany;
+        private string _trackingNumber;
+        public string GetTrackingInfo()
+        {
+            return $"{_shippingCompany}:{_trackingNumber}";
+        }
+
+        public string GetShippingCompany()
+        {
+            return _shippingCompany;
+        }
+        public void SetShippingCompany(string company)
+        {
+            _shippingCompany = company;
+        }
+
+        public string GetTrackingNumber()
+        {
+            return _trackingNumber;
+        }
+        public void SetTrackingNumber(string trackingNumber)
+        {
+            _trackingNumber = trackingNumber;
+        }
+    }
+
+    public class User
+    {
+        public void CreateShipment()
+        {
+            var shipment = new Shipment();
+            shipment.SetShippingCompany("AVendor");
+        }
+    }
+}
---
+++ src/Refactorings/InlineClass/Refactored.cs
@@ -0,0 +1,37 @@
+﻿namespace InlineClass.Original
+{
+    public class TrackingInformation
+    {
+        public string ShippingCompany { get;set; }
+        public string TrackingNumber { get;set; }
+
+        public override string ToString()
+        {
+            return $"{ShippingCompany}:{TrackingNumber}";
+        }
+    }
+    public class Shipment
+    {
+        private TrackingInformation _trackingInformation = new();
+        public string GetTrackingInfo()
+        {
+            return _trackingInformation.ToString();
+        }
+
+        public TrackingInformation GetTrackingInformation() { return _trackingInformation; }
+
+        public void SetTrackingInformation(TrackingInformation aTrackingInformation)
+        {
+            _trackingInformation = aTrackingInformation;
+        }
+    }
+
+    public class User
+    {
+        public void CreateShipment()
+        {
+            var shipment = new Shipment();
+            shipment.GetTrackingInformation().ShippingCompany = "AVendor";
+        }
+    }
+}
