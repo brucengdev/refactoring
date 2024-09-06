2356867e7a16e89872dad35022baacde3cbd8568
brucengdev <brucengdev@gmail.com>
2024-09-06 12:56:11 +0700

    Added example of Hide Delegate

---
+++ src/Refactorings/HideDelegate/Original.cs
@@ -0,0 +1,29 @@
+﻿/**
+ * In this example, Client wants to know a person's manager
+ * and has to access the manager's name through the person's department.
+ * So when the department's implementation change, it affects all client code.
+ * We can reduce this coupling by allow access to the manager directly from Person.
+ **/
+namespace HideDelegate.Original
+{
+    public class Department
+    {
+        public string Manager { get;set; }
+    }
+
+    public class Person
+    {
+        public string Name { get;set; }
+        public Department Department { get;set; }
+    }
+
+    public class Client
+    {
+        public void PrintProfile(Person person)
+        {
+            Console.WriteLine("Name: {0}", person.Name);
+            var manager = person.Department.Manager;
+            Console.WriteLine("Manager: {0}", manager);
+        }
+    }
+}
---
+++ src/Refactorings/HideDelegate/Refactored.cs
@@ -0,0 +1,38 @@
+﻿/**
+ * In this example, Client wants to know a person's manager
+ * and has to access the manager's name through the person's department.
+ * So when the department's implementation change, it affects all client code.
+ * We can reduce this coupling by allow access to the manager directly from Person.
+ **/
+namespace HideDelegate.Refactored
+{
+    public class Department
+    {
+        public string Manager { get;set; }
+    }
+
+    public class Person
+    {
+        public string Name { get; set; }
+        
+        public string Manager
+        {
+            get
+            {
+                return Department.Manager;
+            }
+        }
+
+        private Department Department { get;set; }
+    }
+
+    public class Client
+    {
+        public void PrintProfile(Person person)
+        {
+            Console.WriteLine("Name: {0}", person.Name);
+            var manager = person.Manager;
+            Console.WriteLine("Manager: {0}", manager);
+        }
+    }
+}
