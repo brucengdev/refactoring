afd1eb7f22e35d8d5aa7f457962d488b0b5fc024
brucengdev <brucengdev@gmail.com>
2024-09-10 13:12:04 +0700

    Added example of Move Field

---
+++ src/Refactorings/MoveField/Original.cs
@@ -0,0 +1,46 @@
+﻿/**
+ * Sometimes it looks like a field data belongs somewhere
+ * So you need to move it there
+ **/ 
+namespace MoveField.Original
+{
+    public class Customer
+    {
+        private string _name;
+        private float _discountRate;
+        private CustomerContract _contract;
+        public Customer(string name, float discountRate)
+        {
+            _name = name;
+            _discountRate = discountRate;
+            _contract = new CustomerContract(DateTime.Today);
+        }
+
+        public float DiscountRate
+        {
+            get
+            {
+                return _discountRate;
+            }
+        }
+
+        public void BecomePreferred()
+        {
+            this._discountRate += 0.03f;
+        }
+
+        public float ApplyDiscount(float amount)
+        {
+            return amount - amount * DiscountRate;
+        }
+    }
+
+    public class CustomerContract
+    {
+        private DateTime _startDate;
+        public CustomerContract(DateTime startDate)
+        {
+            _startDate = startDate;
+        }
+    }
+}
---
+++ src/Refactorings/MoveField/Refactored.cs
@@ -0,0 +1,62 @@
+﻿/**
+ * Sometimes it looks like a field data belongs somewhere
+ * So you need to move it there
+ **/ 
+namespace MoveField.Reactored
+{
+    public class Customer
+    {
+        private string _name;
+        private CustomerContract _contract;
+        public Customer(string name, float discountRate)
+        {
+            _name = name;
+            _contract = new CustomerContract(DateTime.Today, discountRate);
+        }
+
+        public float DiscountRate
+        {
+            get
+            {
+                return _contract.DiscountRate;
+            }
+            set
+            {
+                _contract.DiscountRate = value;
+            }
+        }
+
+        public void BecomePreferred()
+        {
+            DiscountRate += 0.03f;
+        }
+
+        public float ApplyDiscount(float amount)
+        {
+            return amount - amount * DiscountRate;
+        }
+    }
+
+    public class CustomerContract
+    {
+        private DateTime _startDate;
+        private float _discountRate;
+        public CustomerContract(DateTime startDate, float discountRate)
+        {
+            _startDate = startDate;
+            _discountRate = discountRate;
+        }
+
+        public float DiscountRate
+        {
+            get
+            {
+                return _discountRate;
+            }
+            set
+            {
+                _discountRate = value;
+            }
+        }
+    }
+}
