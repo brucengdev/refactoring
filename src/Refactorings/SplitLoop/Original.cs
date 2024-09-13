8e9350e36a6a455bf0064072ac15db7d1146a152
brucengdev <brucengdev@gmail.com>
2024-09-13 13:15:06 +0700

    Added example of Split Loop.

---
+++ src/Refactorings/SplitLoop/Original.cs
@@ -0,0 +1,28 @@
+﻿/**
+ * Loops that does single thing is easier to understand and maintain.
+ * When you see loops that does more than one thing, try to split them.
+ **/
+
+namespace SplitLoop.Original
+{
+    public class Person
+    {
+        public int Age { get; set; }
+        public float Salary { get;set; }
+    }
+    public class EmployeeReporter
+    {
+        public void PrintYoungestAndTotalSalary(Person[] people)
+        {
+            float totalSalary = 0f;
+            int youngest = people[0].Age;
+            foreach(var person in people)
+            {
+                if(youngest > person.Age) { youngest = person.Age; }
+                totalSalary += person.Salary;
+            }
+
+            Console.WriteLine("Youngest: {0}, Total salary: {1}", youngest, totalSalary);
+        }
+    }
+}
---
+++ src/Refactorings/SplitLoop/Refactored.cs
@@ -0,0 +1,32 @@
+﻿/**
+ * Duplicate the loop and remove duplicate side effects.
+ * This is technically the end of split loop, but you can do more
+ **/
+
+namespace SplitLoop.Refactored
+{
+    public class Person
+    {
+        public int Age { get; set; }
+        public float Salary { get;set; }
+    }
+    public class EmployeeReporter
+    {
+        public void PrintYoungestAndTotalSalary(Person[] people)
+        {
+            int youngest = people[0].Age;
+            foreach(var person in people)
+            {
+                if(youngest > person.Age) { youngest = person.Age; }
+            }
+
+            float totalSalary = 0f;
+            foreach (var person in people)
+            {
+                totalSalary += person.Salary;
+            }
+
+            Console.WriteLine("Youngest: {0}, Total salary: {1}", youngest, totalSalary);
+        }
+    }
+}
