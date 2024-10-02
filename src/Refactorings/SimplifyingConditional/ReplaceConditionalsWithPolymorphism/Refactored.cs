c60de358116a90fc7fcf233be4f4d0c099716c4b
brucengdev <brucengdev@gmail.com>
2024-10-02 13:32:31 +0700

    WIP

--- src/Refactorings/SimplifyingConditional/ReplaceConditionalsWithPolymorphism/Refactored.cs
+++ src/Refactorings/SimplifyingConditional/ReplaceConditionalsWithPolymorphism/Refactored.cs
@@ -1,64 +1,108 @@
 ﻿namespace ReplaceConditionalsWithPolymorphism
 {
-    internal class Refactored : IRatingCalculator
+
+    internal class VoyageRating
     {
-        public Rating GetRating(Voyage voyage, History history)
+        protected Voyage _voyage;
+        protected History _history;
+        public VoyageRating(Voyage voyage, History history)
         {
-            var vpf = VoyageProfitFactor(voyage, history);
-            var vr = VoyageRisk(voyage);
-            var chr = CaptainHistoryRisk(voyage, history);
+            _voyage = voyage;
+            _history = history;
+        }
+        public Rating GetRating()
+        {
+            var vpf = VoyageProfitFactor();
+            var vr = VoyageRisk();
+            var chr = CaptainHistoryRisk();
             if (vpf * 3 > (vr + chr * 2)) return Rating.A;
             return Rating.B;
         }
-
-        private int VoyageProfitFactor(Voyage voyage, History history)
+        private int VoyageProfitFactor()
         {
             var result = 2;
-            if (voyage.Zone == "china") { result++; }
-            if (voyage.Zone == "east-indies") { result++; }
-            if (voyage.Zone == "china" && HasChina(history))
-            {
-                result += 3;
-                if (history.Voyages.Count > 10) { result++; }
-                if (history.Voyages.Count > 12) { result++; }
-                if (history.Voyages.Count > 18) { result--; }
-            }
-            else
-            {
-                if (history.Voyages.Count > 8) { result++; }
-                if (history.Voyages.Count > 14) { result--; }
-            }
+            if (_voyage.Zone == "china") { result++; }
+            if (_voyage.Zone == "east-indies") { result++; }
+            result += VoyageAndHistoryLengthProfitFactor();
             return result;
         }
 
-        private int VoyageRisk(Voyage voyage)
+        protected virtual int VoyageAndHistoryLengthProfitFactor()
+        {
+            var result = 0;
+            if (_history.Voyages.Count > 8) { result++; }
+            if (_history.Voyages.Count > 14) { result--; }
+            return result;
+        }
+
+        private int VoyageRisk()
         {
             var result = 1;
-            if (voyage.Length > 4) { result += 2; }
-            if (voyage.Length > 8) { result += voyage.Length - 8; }
-            if (new string[] { "china", "east-indies" }.Contains(voyage.Zone))
+            if (_voyage.Length > 4) { result += 2; }
+            if (_voyage.Length > 8) { result += _voyage.Length - 8; }
+            if (new string[] { "china", "east-indies" }.Contains(_voyage.Zone))
             {
                 result += 4;
             }
             return result;
         }
-
-        private int CaptainHistoryRisk(Voyage voyage, History history)
+        protected virtual int CaptainHistoryRisk()
         {
             var result = 1;
-            if (history.Voyages.Count < 5) { result += 4; }
-            result += history.Voyages.Count(v => v.Profit < 0);
-            if (voyage.Zone == "china" && HasChina(history))
-            {
-                result -= 2;
-            }
-
+            if (_history.Voyages.Count < 5) { result += 4; }
+            result += _history.Voyages.Count(v => v.Profit < 0);
             return Math.Max(result, 0);
         }
 
+        private bool HasChina()
+        {
+            return _history.Voyages.Exists(v => v.Zone == "china");
+        }
+    }
+
+    internal class HasExperienceInChinaVoyageRating : VoyageRating
+    {
+        public HasExperienceInChinaVoyageRating(Voyage voyage, History history) : base(voyage, history)
+        {
+        }
+
+        protected override int VoyageAndHistoryLengthProfitFactor()
+        {
+            var result = 0;
+            result += 3;
+            if (_history.Voyages.Count > 10) { result++; }
+            if (_history.Voyages.Count > 12) { result++; }
+            if (_history.Voyages.Count > 18) { result--; }
+            return result;
+        }
+
+        protected override int CaptainHistoryRisk()
+        {
+            return Math.Max(base.CaptainHistoryRisk() - 2, 0);
+        }
+    }
+
+    internal class Refactored : IRatingCalculator
+    {
+        private VoyageRating CreateVoyageRating(Voyage voyage, History history)
+        {
+            if(voyage.Zone == "china" && HasChina(history))
+            {
+                return new HasExperienceInChinaVoyageRating(voyage, history);
+            }
+            return new VoyageRating(voyage, history);
+        }
         private bool HasChina(History history)
         {
             return history.Voyages.Exists(v => v.Zone == "china");
+        }
+
+        public Rating GetRating(Voyage voyage, History history)
+        {
+            var rating = CreateVoyageRating(voyage, history);
+
+            return rating.GetRating();
+
         }
     }
 }
