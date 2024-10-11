825e4376310afc5e9e3d316f381b453da2434741
brucengdev <brucengdev@gmail.com>
2024-10-11 13:05:16 +0700

    Added original code

---
+++ src/Refactorings/RefactoringAPIs/SeparateQueryFromModifier/Original/MiscreantAlerter.cs
@@ -0,0 +1,33 @@
+﻿namespace SeparateQueryFromModifier.Original
+{
+    internal class MiscreantAlerter
+    {
+        private readonly IAlarm _alarm;
+        public MiscreantAlerter(IAlarm alarm)
+        {
+            _alarm = alarm;
+        }
+        public string AlertForMiscreant(List<string> people)
+        {
+            foreach(var person in people)
+            {
+                if(person == "Don")
+                {
+                    SetOffAlarms();
+                    return "Don";
+                }
+                if(person == "John")
+                {
+                    SetOffAlarms();
+                    return "John";
+                }
+            }
+            return "";
+        }
+
+        private void SetOffAlarms()
+        {
+            _alarm.Start();
+        }
+    }
+}
---
+++ src/Refactorings/RefactoringAPIs/SeparateQueryFromModifier/Original/Monitor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeparateQueryFromModifier.Original
+{
+    //monitor the people in the room and fire an alarm
+    //when a miscreant is found
+    internal class Monitor
+    {
+        private readonly MiscreantAlerter _alerter;
+        private readonly IMessenger _messenger;
+
+        public Monitor(IAlarm alarm, IMessenger messenger)
+        {
+            _alerter = new MiscreantAlerter(alarm);
+            _messenger = messenger;
+        }
+        public void PeopleChanged(List<string> currentPeople)
+        {
+            var miscreant = _alerter.AlertForMiscreant(currentPeople);
+            if (miscreant != "")
+            {
+                _messenger.SendMessage("Miscreant " + miscreant + " found!");
+            }
+        }
+    }
+}
