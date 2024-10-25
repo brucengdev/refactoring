4c15e493f90d20000e30e19e7ac0d7f46b7815c1
brucengdev <brucengdev@gmail.com>
2024-10-25 13:04:29 +0700

    Fix test run error.

--- src/Refactorings/RefactoringAPIs/ReplaceFunctionWithCommand/Refactored/Program.cs
+++ src/Refactorings/RefactoringAPIs/ReplaceFunctionWithCommand/Refactored/Program.cs
@@ -2,11 +2,6 @@
 {
     internal class Program
     {
-        public static void Main(string[] args)
-        {
-            //bunch of stuff
-        }
-
         public int Score(Candidate candidate, MedicalExam medicalExam, ScoringGuide scoringGuide)
         {
             return new Scorer(candidate, medicalExam, scoringGuide).Execute();
