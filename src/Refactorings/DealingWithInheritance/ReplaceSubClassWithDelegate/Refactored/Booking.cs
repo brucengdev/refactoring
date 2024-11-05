104de183d563d7aa69bee93452822697551d9a4b
brucengdev <brucengdev@gmail.com>
2024-11-05 13:07:44 +0700

    Duplicate to Refactored code.

---
+++ src/Refactorings/DealingWithInheritance/ReplaceSubClassWithDelegate/Refactored/Booking.cs
@@ -0,0 +1,33 @@
+﻿namespace ReplaceSubClassWithDelegate.Refactored
+{
+    internal class Booking
+    {
+        protected Show _show { get; }
+        protected DateTime _date { get; }
+        public Booking(Show show, DateTime date, bool isPeakDay)
+        {
+            _show = show;
+            _date = date;
+            IsPeakDay = isPeakDay;
+        }
+
+        public bool IsPeakDay { get; set; }
+        public virtual bool HasTalkback { 
+            get {
+                return _show.HasTalkBack && IsPeakDay;
+            }
+        }
+
+        public virtual double BasePrice
+        {
+            get
+            {
+                var result = _show.Price;
+                if(IsPeakDay) {
+                    result += Math.Round(_show.Price * 0.15);
+                }
+                return result;
+            }
+        }
+    }
+}
---
+++ src/Refactorings/DealingWithInheritance/ReplaceSubClassWithDelegate/Refactored/Show.cs
@@ -0,0 +1,11 @@
+﻿namespace ReplaceSubClassWithDelegate.Refactored
+{
+    internal class Show
+    {
+        public bool HasTalkBack { get; set; }
+
+        public double Price { get; set; }
+
+        public bool HasDinner { get; set; }
+    }
+}
