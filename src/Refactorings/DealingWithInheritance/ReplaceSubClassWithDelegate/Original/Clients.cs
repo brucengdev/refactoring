3d253009d112f4b0184f361be1afbc3850c92f27
brucengdev <brucengdev@gmail.com>
2024-11-05 13:06:15 +0700

    Added original code with tests.

---
+++ src/Refactorings/DealingWithInheritance/ReplaceSubClassWithDelegate/Original/Clients.cs
@@ -0,0 +1,28 @@
+﻿namespace ReplaceSubClassWithDelegate.Original
+{
+    internal class Clients
+    {
+        public string GetTicketInfos(Show show, DateTime date, bool isPeak)
+        {
+            var booking = new Booking(show, date, isPeak);
+            return $"Booking\n" +
+                $"Talkback: {booking.HasTalkback}\n" +
+                $"Peak day?: {booking.IsPeakDay}\n" +
+                $"Base price: {booking.BasePrice}";
+        }
+
+        public string GetTicketInfos(
+            Show show, 
+            DateTime date, 
+            bool isPeak, 
+            Extras extras)
+        {
+            var booking = new PremiumBooking(show, date, isPeak, extras);
+            return $"Premium booking\n" +
+                $"Talkback: {booking.HasTalkback}\n" +
+                $"Dinner: {booking.HasDinner}\n" +
+                $"Peak day?: {booking.IsPeakDay}\n" +
+                $"Base price: {booking.BasePrice}";
+        }
+    }
+}
---
+++ src/Refactorings/DealingWithInheritance/ReplaceSubClassWithDelegate/Original/Show.cs
@@ -0,0 +1,11 @@
+﻿namespace ReplaceSubClassWithDelegate.Original
+{
+    internal class Show
+    {
+        public bool HasTalkBack { get; set; }
+
+        public double Price { get; set; }
+
+        public bool HasDinner { get; set; }
+    }
+}
